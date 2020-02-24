83ce1ef0a35d15d3748834ed780d631846704117
zhujian <505169307@qq.com>
2020-02-24 13:23:57 +0800

    perf(data): 下载VOC 2007数据集，解析并显示

--- py/create_data.py
+++ py/create_data.py
@@ -6,6 +6,8 @@
 @time:   2020-02-24
 """
 
+import cv2
+import numpy as np
 from torchvision.datasets import VOCDetection
 
 if __name__ == '__main__':
@@ -13,3 +15,12 @@ if __name__ == '__main__':
     下载PASCAL VOC数据集
     """
     dataset = VOCDetection('./data', year='2007', image_set='trainval', download=True)
+
+    img, target = dataset.__getitem__(1000)
+    img = np.array(img)
+
+    print(target)
+    print(img.shape)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
