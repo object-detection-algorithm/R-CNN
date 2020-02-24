752a04bac57a493cecdbf01eae158627ff699506
zhujian <505169307@qq.com>
2020-02-24 12:51:24 +0800

    feat(python): 创建VOC数据集

--- py/create_data.py
+++ py/create_data.py
@@ -5,3 +5,11 @@
 @file:   create_data.py
 @time:   2020-02-24
 """
+
+from torchvision.datasets import VOCDetection
+
+if __name__ == '__main__':
+    """
+    下载PASCAL VOC数据集
+    """
+    dataset = VOCDetection('./data', year='2007', image_set='trainval', download=True)
