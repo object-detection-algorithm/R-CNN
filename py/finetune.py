0a9364bc8ea3303cdb67f16577af461c08d9c4f4
zhujian <505169307@qq.com>
2020-03-01 10:51:10 +0800

    feat(finetune): 微调CNN实现

---
+++ py/finetune.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/3/1 上午9:54
+@file: finetune.py
+@author: zj
+@description: 
+"""
+
+import os
+import copy
+import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+import torchvision.models as models
+
+
+def load_data(data_root_dir):
+    transform = transforms.Compose([
+        transforms.Resize((227, 227)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    data_loaders = {}
+    data_sizes = {}
+    for name in ['train', 'val']:
+        data_path = os.path.join(data_root_dir, name)
+        data_set = ImageFolder(data_path, transform=transform)
+        data_loader = DataLoader(data_set, batch_size=128, shuffle=True, num_workers=8)
+
+        data_loaders[name] = data_loader
+        data_sizes[name] = len(data_set)
+
+    return data_loaders, data_sizes
+
+
+def train_model(data_loaders, model, criterion, optimizer, lr_scheduler, num_epochs=25, device=None):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in data_loaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                lr_scheduler.step()
+
+            epoch_loss = running_loss / data_sizes[phase]
+            epoch_acc = running_corrects.double() / data_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_loaders, data_sizes = load_data('./data/finetune_car')
+    # print(data_loaders)
+    # print(data_sizes)
+
+    model = models.alexnet(pretrained=True)
+    # print(model)
+    num_features = model.classifier[6].in_features
+    model.classifier[6] = nn.Linear(num_features, 2)
+    # print(model)
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+    train_model(data_loaders, model, criterion, optimizer, lr_scheduler, device=device, num_epochs=25)
