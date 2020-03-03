c5977bf3054d0f03981f03bfe23c9cbb509f4bf1
zhujian <505169307@qq.com>
2020-03-03 16:41:17 +0800

    refactor(data): 计算单次解析的时间

--- py/utils/data/dataset_finetune.py
+++ py/utils/data/dataset_finetune.py
@@ -7,6 +7,7 @@
 @description: 创建微调数据集
 """
 
+import time
 import numpy as np
 import cv2
 import os
@@ -21,11 +22,12 @@ from utils.util import compute_ious
 car_root_dir = '../../data/voc_car/'
 finetune_root_dir = '../../data/finetune_car/'
 
+
 def parse_annotation_jpeg(samples, annotation_dir, jpeg_dir, dst_root_dir, gs):
     """
     获取正负样本（注：忽略属性difficult为True的标注边界框）
     正样本：候选建议与标注边界框IoU大于等于0.5
-    负样本：IoU大于0,小于0.5。为了限制负样本数目，其大小必须大于标注框的1/5
+    负样本：IoU大于0,小于0.5。为了进一步限制负样本数目，其大小必须大于标注框的1/5
     """
     dst_positive_dir = os.path.join(dst_root_dir, '1')
     dst_nevative_dir = os.path.join(dst_root_dir, '0')
@@ -33,6 +35,8 @@ def parse_annotation_jpeg(samples, annotation_dir, jpeg_dir, dst_root_dir, gs):
     check_dir(dst_nevative_dir)
 
     for sample_name in samples:
+        since = time.time()
+
         annotation_path = os.path.join(annotation_dir, sample_name + '.xml')
         jpeg_path = os.path.join(jpeg_dir, sample_name + '.jpg')
 
@@ -69,6 +73,9 @@ def parse_annotation_jpeg(samples, annotation_dir, jpeg_dir, dst_root_dir, gs):
                 cv2.imwrite(dst_negative_path, rect_img)
             else:
                 pass
+
+        time_elapsed = time.time() - since
+        print('parse {}.png in {:.0f}m {:.0f}s'.format(sample_name, time_elapsed // 60, time_elapsed % 60))
 
 
 if __name__ == '__main__':
