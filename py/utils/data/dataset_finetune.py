33ab0b2f094908a3f84cc883d207d6da216c1367
zhujian <505169307@qq.com>
2020-03-03 18:55:00 +0800

    perf(data): 修改微调数据集的正负样本保存格式

--- py/utils/data/dataset_finetune.py
+++ py/utils/data/dataset_finetune.py
@@ -8,6 +8,7 @@
 """
 
 import time
+import shutil
 import numpy as np
 import cv2
 import os
@@ -19,79 +20,95 @@ from utils.util import parse_xml
 from utils.util import iou
 from utils.util import compute_ious
 
-car_root_dir = '../../data/voc_car/'
-finetune_root_dir = '../../data/finetune_car/'
 
-
-def parse_annotation_jpeg(samples, annotation_dir, jpeg_dir, dst_root_dir, gs):
+def parse_annotation_jpeg(annotation_path, jpeg_path, gs):
     """
     获取正负样本（注：忽略属性difficult为True的标注边界框）
     正样本：候选建议与标注边界框IoU大于等于0.5
     负样本：IoU大于0,小于0.5。为了进一步限制负样本数目，其大小必须大于标注框的1/5
     """
-    dst_positive_dir = os.path.join(dst_root_dir, '1')
-    dst_nevative_dir = os.path.join(dst_root_dir, '0')
-    check_dir(dst_positive_dir)
-    check_dir(dst_nevative_dir)
+    img = cv2.imread(jpeg_path)
 
-    for sample_name in samples:
-        since = time.time()
+    selectivesearch.config(gs, img, strategy='q')
+    # 计算候选建议
+    rects = selectivesearch.get_rects(gs)
+    # 获取标注边界框
+    bndboxs = parse_xml(annotation_path)
 
-        annotation_path = os.path.join(annotation_dir, sample_name + '.xml')
-        jpeg_path = os.path.join(jpeg_dir, sample_name + '.jpg')
+    # 标注框大小
+    maximum_bndbox_size = 0
+    for bndbox in bndboxs:
+        xmin, ymin, xmax, ymax = bndbox
+        bndbox_size = (ymax - ymin) * (xmax - xmin)
+        if bndbox_size > maximum_bndbox_size:
+            maximum_bndbox_size = bndbox_size
 
-        img = cv2.imread(jpeg_path)
-        selectivesearch.config(gs, img, strategy='q')
+    # 获取候选建议和标注边界框的IoU
+    iou_list = compute_ious(rects, bndboxs)
 
-        # 计算候选建议
-        rects = selectivesearch.get_rects(gs)
-        # 获取标注边界框
-        bndboxs = parse_xml(annotation_path)
+    positive_list = list()
+    negative_list = list()
+    for i in range(len(iou_list)):
+        xmin, ymin, xmax, ymax = rects[i]
+        rect_size = (ymax - ymin) * (xmax - xmin)
 
-        maximum_bndbox_size = 0
-        for bndbox in bndboxs:
-            xmin, ymin, xmax, ymax = bndbox
-            bndbox_size = (ymax - ymin) * (xmax - xmin)
-            if bndbox_size > maximum_bndbox_size:
-                maximum_bndbox_size = bndbox_size
+        iou_score = iou_list[i]
+        if iou_list[i] >= 0.5:
+            # 正样本
+            positive_list.append(rects[i])
+        if 0 < iou_list[i] < 0.5 and rect_size > maximum_bndbox_size / 5.0:
+            # 负样本
+            negative_list.append(rects[i])
+        else:
+            pass
 
-        # 获取候选建议和标注边界框的IoU
-        iou_list = compute_ious(rects, bndboxs)
-        for i in range(len(iou_list)):
-            xmin, ymin, xmax, ymax = rects[i]
-            rect_size = (ymax - ymin) * (xmax - xmin)
-            rect_img = img[ymin:ymax, xmin:xmax]
-
-            iou_score = iou_list[i]
-            if iou_list[i] >= 0.5:
-                # 正样本
-                dst_positive_path = os.path.join(dst_positive_dir, '%s-%d.png' % (sample_name, i))
-                cv2.imwrite(dst_positive_path, rect_img)
-            if 0 < iou_list[i] < 0.5 and rect_size > maximum_bndbox_size / 5.0:
-                # 负样本
-                dst_negative_path = os.path.join(dst_nevative_dir, '%s-%d.png' % (sample_name, i))
-                cv2.imwrite(dst_negative_path, rect_img)
-            else:
-                pass
-
-        time_elapsed = time.time() - since
-        print('parse {}.png in {:.0f}m {:.0f}s'.format(sample_name, time_elapsed // 60, time_elapsed % 60))
+    return positive_list, negative_list
 
 
 if __name__ == '__main__':
-    gs = selectivesearch.get_selective_search()
-
+    car_root_dir = '../../data/voc_car/'
+    finetune_root_dir = '../../data/finetune_car/'
     check_dir(finetune_root_dir)
-    for name in ['train', 'val']:
-        data_root_dir = os.path.join(car_root_dir, name)
-        samples = parse_car_csv(data_root_dir)
-        # print(samples)
 
-        data_annotation_dir = os.path.join(data_root_dir, 'Annotations')
-        data_jpeg_dir = os.path.join(data_root_dir, 'JPEGImages')
+    gs = selectivesearch.get_selective_search()
+    for name in ['train', 'val']:
+        src_root_dir = os.path.join(car_root_dir, name)
+        src_annotation_dir = os.path.join(src_root_dir, 'Annotations')
+        src_jpeg_dir = os.path.join(src_root_dir, 'JPEGImages')
 
         dst_root_dir = os.path.join(finetune_root_dir, name)
+        dst_annotation_dir = os.path.join(dst_root_dir, 'Annotations')
+        dst_jpeg_dir = os.path.join(dst_root_dir, 'JPEGImages')
         check_dir(dst_root_dir)
-        parse_annotation_jpeg(samples, data_annotation_dir, data_jpeg_dir, dst_root_dir, gs)
+        check_dir(dst_annotation_dir)
+        check_dir(dst_jpeg_dir)
 
+        total_num_positive = 0
+        total_num_negative = 0
+
+        samples = parse_car_csv(src_root_dir)
+        for sample_name in samples:
+            since = time.time()
+
+            src_annotation_path = os.path.join(src_annotation_dir, sample_name + '.xml')
+            src_jpeg_path = os.path.join(src_jpeg_dir, sample_name + '.jpg')
+            # 获取正负样本
+            positive_list, negative_list = parse_annotation_jpeg(src_annotation_path, src_jpeg_path, gs)
+            total_num_positive += len(positive_list)
+            total_num_negative += len(negative_list)
+
+            dst_annotation_positive_path = os.path.join(dst_annotation_dir, sample_name + '_1' + '.csv')
+            dst_annotation_negative_path = os.path.join(dst_annotation_dir, sample_name + '_0' + '.csv')
+            dst_jpeg_path = os.path.join(dst_jpeg_dir, sample_name + '.jpg')
+            # 保存图片
+            shutil.copyfile(src_jpeg_path, dst_jpeg_path)
+            # 保存正负样本标注
+            np.savetxt(dst_annotation_positive_path, np.array(positive_list), fmt='%d', delimiter=' ')
+            np.savetxt(dst_annotation_negative_path, np.array(negative_list), fmt='%d', delimiter=' ')
+
+            time_elapsed = time.time() - since
+            print('parse {}.png in {:.0f}m {:.0f}s'.format(sample_name, time_elapsed // 60, time_elapsed % 60))
+
+    print('positive num: %d' % (total_num_positive))
+    print('negative num: %d' % (total_num_negative))
     print('done')
