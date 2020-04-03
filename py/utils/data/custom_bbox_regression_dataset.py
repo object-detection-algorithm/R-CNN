22559514bea1405b41dec58d86e32ca957a18754
zhujian <505169307@qq.com>
2020-04-03 21:05:13 +0800

    feat(dataset): 自定义边界框数据集类
    1. __len__返回正样本边界框数目
     2. __getiem__返回正样本边界框图像以及对应标注边界框

---
+++ py/utils/data/custom_bbox_regression_dataset.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/4/3 下午8:07
+@file: custom_bbox_regression_dataset.py
+@author: zj
+@description:
+"""
+
+import os
+import cv2
+import numpy as np
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import utils.util as util
+
+
+class BBoxRegressionDataset(Dataset):
+
+    def __init__(self, root_dir, transform=None):
+        super(BBoxRegressionDataset, self).__init__()
+        self.transform = transform
+
+        samples = util.parse_car_csv(root_dir)
+        jpeg_list = list()
+        # 保存{'image_id': ?, 'positive': ?, 'bndbox': ?}
+        box_list = list()
+        for i in range(len(samples)):
+            sample_name = samples[i]
+
+            jpeg_path = os.path.join(root_dir, 'JPEGImages', sample_name + '.jpg')
+            bndbox_path = os.path.join(root_dir, 'bndboxs', sample_name + '.csv')
+            positive_path = os.path.join(root_dir, 'positive', sample_name + '.csv')
+
+            jpeg_list.append(cv2.imread(jpeg_path))
+            bndboxes = np.loadtxt(bndbox_path, dtype=np.int, delimiter=' ')
+            positives = np.loadtxt(positive_path, dtype=np.int, delimiter=' ')
+
+            for positive in positives:
+                bndbox = self.get_bndbox(bndboxes, positive)
+                box_list.append({'image_id': i, 'positive': positive, 'bndbox': bndbox})
+
+        self.jpeg_list = jpeg_list
+        self.box_list = box_list
+
+    def __getitem__(self, index: int):
+        assert index < self.__len__(), '数据集大小为%d，当前输入下标为%d' % (self.__len__(), index)
+
+        box_dict = self.box_list[index]
+        image_id = box_dict['image_id']
+        positive = box_dict['positive']
+        bndbox = box_dict['bndbox']
+
+        jpeg_img = self.jpeg_list[image_id]
+        xmin, ymin, xmax, ymax = positive
+        image = jpeg_img[ymin:ymax, xmin:xmax]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, bndbox
+
+    def __len__(self):
+        return len(self.box_list)
+
+    def get_bndbox(self, bndboxes, positive):
+        """
+        返回和positive的IoU最大的标注边界框
+        :param bndboxes: 大小为[N, 4]或者[4]
+        :param positive: 大小为[4]
+        :return: [4]
+        """
+
+        if len(bndboxes.shape) == 1:
+            # 只有一个标注边界框，直接返回即可
+            return bndboxes
+        else:
+            scores = util.iou(positive, bndboxes)
+            return bndboxes[np.argmax(scores)]
+
+
+def test():
+    """
+    创建数据集类实例
+    """
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((227, 227)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    data_root_dir = '../../data/bbox_regression'
+    data_set = BBoxRegressionDataset(data_root_dir, transform=transform)
+
+    print(data_set.__len__())
+    image, bndbox = data_set.__getitem__(10)
+    print(image.shape)
+    print(bndbox)
+
+
+def test2():
+    """
+    测试DataLoader使用
+    """
+    transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.Resize((227, 227)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    data_root_dir = '../../data/bbox_regression'
+    data_set = BBoxRegressionDataset(data_root_dir, transform=transform)
+    data_loader = DataLoader(data_set, batch_size=128, shuffle=True, num_workers=8)
+
+    items = next(data_loader.__iter__())
+    datas, targets = items
+    print(datas.shape)
+    print(targets.shape)
+
+
+if __name__ == '__main__':
+    test()
